f0f8adf12147fb122191d36ce2d32527539a670d
anna-kapitonova <48308650+anna-kapitonova@users.noreply.github.com>
2021-10-31 18:08:14 +0300

    Corrected code according to Roma's comments
    Solved the problem with gc_bounds and length_bounds of one-number input. Solved quality-threshold filtration problem.

--- HW3_fastq_filtrator/fastq_filtrator.py
+++ HW3_fastq_filtrator/fastq_filtrator.py
@@ -2,10 +2,6 @@
 
 def length_and_gc_count_filter(file_path, length_bounds, gc_bounds,
                                save_filtered, good_lines, bad_lines):
-    if type(length_bounds) != tuple:
-        length_bounds = (0, length_bounds)
-    elif type(gc_bounds) != tuple:
-        gc_bounds = (0, gc_bounds)
     with open(file_path) as file:
         counter = 2  # counter for second lines with nucleic acid sequence
         number = 0  # ordinal number
@@ -14,7 +10,7 @@ def length_and_gc_count_filter(file_path, length_bounds, gc_bounds,
             number += 1
             if counter == 4:
                 counter = 0
-                seq = line.strip()
+                seq = line.upper().strip()
                 gc_content = round(((seq.count('C') + seq.count('G'))/len(seq)*100), 1)  # count gc-content
                 if length_bounds[0] <= len(seq) <= length_bounds[1] and gc_bounds[0] <= gc_content <= gc_bounds[1]:
                     good_lines.add(number - 1)  # add all four lines for one sequence
@@ -45,7 +41,7 @@ def quality_filter(file_path, output_file_good, output_file_bad, quality_thresho
                 for symbol in quality_seq:
                     sum_quality += (ord(symbol) - 33)  # count Q-score
                 mean_quality = sum_quality/len(quality_seq)
-                if mean_quality <= quality_threshold and number in good_lines:
+                if mean_quality < quality_threshold and number in good_lines:
                     good_lines.remove(number - 3)
                     good_lines.remove(number - 2)
                     good_lines.remove(number - 1)
@@ -97,10 +93,18 @@ print("Enter file prefix to output file (without .fastq extension)")
 output_file_prefix = input()
 print("Do you want to set filter parameters manually? y/n")
 if input() == "y":
-    print("Enter gc_content_bounds: two numbers separated by white space (default 0, 100)")
-    gc_bounds = tuple((int(i)) for i in input().split(" "))
-    print("Enter length_bounds: two numbers separated by white space (default 0, 2**32)")
-    length_bounds = tuple((int(i)) for i in input().split(" "))
+    print("Enter gc_content_bounds: two numbers separated by white space or one upper-bound number (default 0, 100)")
+    gc_bounds = [int(i) for i in input().split(" ")]
+    if len(gc_bounds) == 1:
+        gc_bounds = (0, gc_bounds[0])
+    else:
+        gc_bounds = (gc_bounds[0], gc_bounds[1])
+    print("Enter length_bounds: two numbers separated by white space or one upper-bound number(default 0, 2**32)")
+    length_bounds = [int(i) for i in input().split(" ")]
+    if len(length_bounds) == 1:
+        length_bounds = (0, length_bounds[0])
+    else:
+        length_bounds = (length_bounds[0], length_bounds[1])
     print("Enter quality_threshold: one number from 0 to 40 (default 0)")
     quality_threshold = int(input())
     print("Enter save_filtered argument (True/False, default False)")
