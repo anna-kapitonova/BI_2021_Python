271691b54c35e750092873dc1587c986b9aedb7e
Anna Kapitonova <48308650+anna-kapitonova@users.noreply.github.com>
2021-12-14 02:41:23 +0300

    Update sort.py

--- HW7_UNIX_tools/sort.py
+++ HW7_UNIX_tools/sort.py
@@ -1,21 +1,24 @@
 #! /usr/bin/env python
 import sys
-import os
 import argparse
+
 
 def sort_tool(file):
     with open(file) as f:
         f_lines = f.read().splitlines()
         f_lines.sort()
     return('\n'.join(f_lines))
-    
+
+
 def main():
     print(sort_tool(args.file))
-    
+
+
 if not sys.stdin.isatty():
     input_file = sys.stdin.read().splitlines()
     input_file.sort()
     print('\n'.join(input_file))
+
 
 # otherwise, read the given filename                                            
 else:
