dcc3339d62722c225aa3e83826bfbbbf82b5dc99
Anna Kapitonova <48308650+anna-kapitonova@users.noreply.github.com>
2021-12-14 02:42:20 +0300

    Update rm.py

--- HW7_UNIX_tools/rm.py
+++ HW7_UNIX_tools/rm.py
@@ -7,16 +7,18 @@ parser = argparse.ArgumentParser(description="UNIX tool rm analog: remove direct
 parser.add_argument("-r", action='store_true',
                     help="Attempt to remove the file hierarchy rooted in each file argument")
 parser.add_argument('input_names', nargs='+', help='print the file- or dirnames')
-args = parser.parse_args() 
-    
+args = parser.parse_args()
+
+
 def main():
     for name in args.input_names:
-        if os.path.isfile(name) == True:
-            os.remove(name) 
+        if os.path.isfile(name) is True:
+            os.remove(name)
         else:
-            if args.r == True:
+            if args.r is True:
                 shutil.rmtree(name)
             else:
                 print("rm: "+name+": is a directory")
+
 
 main()
